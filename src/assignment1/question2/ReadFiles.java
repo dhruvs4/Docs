6f944d5afbc3322d0be084a0604a1a410aa4beae
dhruv <dhruvsharma9997@gmail.com>
2018-07-25 09:57:51 +0530

    first commit

--- src/assignment1/question2/ReadFiles.java
+++ src/assignment1/question2/ReadFiles.java
@@ -1,18 +1,21 @@
 package assignment1.question2;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLOutput;
 
 public class ReadFiles {
     public static void main(String[] args){
         if(args.length>0){
             for(int i=0;i<args.length;i++){
                 try {
-                    BufferedReader reader = new BufferedReader(new FileReader(args[i])) ;
-                    int lines=0 ;
-                    while(reader.readLine() != null) lines++ ;
-                    System.out.println("File name: " + args[i] + ", Lines:"+ lines); ;
-                    reader.close() ;
+                    int lines = count_lines(args[i]) ;
+                    if(lines!=1) {
+                        System.out.println("File name: " + args[i] + ", Lines:"+ lines); ;
+                    }
+                    else System.out.println("error in file");
                 }
                 catch(Exception e){
                     System.out.println(e);
@@ -20,4 +23,17 @@ public class ReadFiles {
             }
         }
     }
+    static int count_lines(String f){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(f)) ;
+            int lines=0 ;
+            while(reader.readLine() != null) lines++ ;
+            reader.close() ;
+            return lines ;
+        }
+        catch(Exception e){
+            return -1 ;
+        }
+    }
 }
+
